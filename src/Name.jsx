7087a425a5f1161cb230b805aa6e1c38d3e788a9
Onelast12 <fghggh737@gmail.com>
2025-07-31 13:18:46 +0700

    nambah file name

---
+++ src/Name.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+let nama = "Bob";
+
+const Name = () => {
+  return (
+    <div>
+      <h1>Nama Saya { nama }</h1>
+    </div>
+  )
+}
+
+export default Name
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Name from './Name.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Name />
     <App />
   </StrictMode>,
 )
